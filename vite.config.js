e405409dd118d4e7f45de637aea794e3a9c6b9d7
Elahe <elasdmomen@gmail.com>
2025-11-28 19:02:55 +0330

    deploy: add build files and config updates

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  base: '/dastres/',
+  plugins: [react(),tailwindcss()],
+  base: '/dastresiiend/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
+  }
 })
