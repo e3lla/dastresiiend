56de4724b3c3874d06ba06e5d42e2ba5c0221a0d
Elahe <elasdmomen@gmail.com>
2025-11-29 14:55:28 +0330

    mi

--- src/Api/ArticlesApi.jsx
+++ src/Api/ArticlesApi.jsx
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+const API_URL = import.meta.env.VITE_API_URL || 'https://localhost:3001';
 
 export const fetchArticlesAPI = async () => {
   try {
--- src/components/LastArticle/LastArticleSlice.jsx
+++ src/components/LastArticle/LastArticleSlice.jsx
@@ -1,12 +1,10 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
-
 export const fetchArticles = createAsyncThunk(
   'articles/fetchArticles',
   async (_, { rejectWithValue }) => {
     try {
-      const response = await fetch(`${API_URL}/articles`);
+      const response = await fetch('https://localhost:3001/articles');
       if (!response.ok) throw new Error(`خطای HTTP: ${response.status}`);
       const data = await response.json();
       return data;
